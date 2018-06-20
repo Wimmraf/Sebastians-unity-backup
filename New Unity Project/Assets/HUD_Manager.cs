e64743ec8e32f68c701a7f24f451f381eb4c6689
Wimmraf <sebastianp04@yahoo.com>
2018-06-20 11:53:08 -0400

    6/20/2018
    Former-commit-id: 82aa978a89954a122765853f8530b93c2299dce0

---
+++ New Unity Project/Assets/HUD_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Manager : MonoBehaviour
+{
+
+    int score;
+    public Text scoretext;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        scoretext.text = "Score: " + score;
+    }
+
+    void UpdateScore(int amount)
+    {
+        score = score + amount;
+        scoretext.text = "Score:" + score;
+    }
+}   
+
+
