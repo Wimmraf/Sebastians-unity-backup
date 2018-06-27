5ad84373866208d382ba204d107e4889b81d4836
Wimmraf <sebastianp04@yahoo.com>
2018-06-27 11:53:23 -0400

    6/27/2018
    Former-commit-id: 7298ad7b8e8fd16b109652da675294ded874ce09

---
+++ New Unity Project/Assets/SCRIPTS/collectiblescript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectiblescript : MonoBehaviour {
+    public int points;
+    HUD_Manager hudmanager;
+
+	// Use this for initialization
+	void Start () {
+        hudmanager = GameObject.FindGameObjectWithTag("Player").GetComponent<HUD_Manager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            hudmanager.UpdateScore(points);
+        }
+    }
+}
